6782a4637626e83838eb3682c80f83d0c357e6cd
Tanmay Kaushik <96286116+tanmaykaushikk@users.noreply.github.com>
2024-02-06 17:13:46 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,32 @@
+// let score = "33ed";
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+
+// In Number Conversion:-
+// 33  =>  number
+// "33"  => string
+// "33abs"  => type --> number ,, console --> NaN
+// true  => 1 , false => 0
+
+// let isLoggedIn = 0
+// let value = Boolean(isLoggedIn)
+// console.log(value)
+
+// In Boolean conversion:-
+//  1 => true , 0 => false
+// "" => false 
+// // "tanmay" => true
+
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof someNumber)
+// console.log(typeof stringNumber)
+
