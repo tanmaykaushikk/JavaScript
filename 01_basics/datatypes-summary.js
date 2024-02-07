9dce61480b100c79813a59b086c87b92171ddf61
Tanmay Kaushik <96286116+tanmaykaushikk@users.noreply.github.com>
2024-02-07 17:54:40 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+//Primitive (call by value , reference nhi diya jata ek copy di jati h inki)
+
+// 7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
+
+// Js is dynamically type language.
+
+
+
+// reference Type /Non primitive
+
+// Arrays, Objects, Funcitons
+
+
+const names = ["tanmay" , "jai", "nabh"];
+
+let myObj = {               // objects curly brackets ke ander hote hain.
+    name:"tanmay",
+    age: 21,
+}
+
+
+const myFunction = function(){
+console.log("hello world");
+}
+
+
+
+
+
