6cc85a390a3bb259e1e4cad69caf206813a362b0
Tanmay Kaushik <96286116+tanmaykaushikk@users.noreply.github.com>
2024-02-09 13:55:23 +0000

    scopes basic

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,60 @@
+//var c = 300                           // var doesn't respect scopes.
+let a = 300
+if (true) {
+    let a = 10                     //{}  braces ke ander ka block scope bahar wala global scope.
+    const b = 20
+    // console.log("INNER: ", a);
+    
+} 
+
+//    Node enviournment mai global scope alag hai aur browser par yeh alag hai.
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
